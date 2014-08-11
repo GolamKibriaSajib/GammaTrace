01fcc84124aa1e10cee8d15cdae7f1cf80766ba1
abrarmusa <abrar.musa.89@gmail.com>
2014-08-11 12:15:36 -0700

    red graph

--- app/controllers/interest_swaps_controller.rb
+++ app/controllers/interest_swaps_controller.rb
@@ -6,14 +6,11 @@ class InterestSwapsController < ApplicationController
     @chart = LazyHighCharts::HighChart.new('graph') do |f|
       f.title(:text => "Time Series")
       f.subtitle(:text => "Click and drag in the plot area to zoom in. Pinch the chart to zoom in")
-      f.xAxis(:type => "datetime", :floor => "2014")
+      f.xAxis(:gridLineWidth => 0,:type => "datetime", :floor => "2014")
       f.series(:color => "red", :data => @fixedfloatswaps.map {|x| [((x.execution_timestamp)*1000), x.price_notation, x.dissemination_id]})
-      f.tooltip(:valueSuffix => "%")
-      f.yAxis [
-        {:title => {:text => "Price Notation in %", :margin => 1}}
-      ]
+      f.tooltip(crosshairs: [true,true],:valueSuffix => "%")
+      f.yAxis(:gridLineWidth => 0,:type => "linear",:title => {:text => "Price Notation in %"})
       f.rangeSelector(:selected => 1)
-
       # f.legend(:align => 'right', :verticalAlign => 'top', :y => 5, :x => -10, :layout => 'vertical',)
       f.chart({:type =>"spline", :zoomType => 'xy', :animation => true})
     end
@@ -22,7 +19,7 @@ class InterestSwapsController < ApplicationController
     @chart2 = LazyHighCharts::HighChart.new('graph') do |f|
       f.title(:text => "Scatter Plot")
       f.subtitle(:text => "Click and drag in the plot area to zoom in. Pinch the chart to zoom in")
-      f.xAxis(:type => "linear", :floor => 0, :title => {:text=>"(END_DATE - today) in years"})
+      f.xAxis(:gridLineWidth=> 0, :type => "linear", :floor => 0, :title => {:text=>"(END_DATE - today) in years"})
       f.series(:color => "red",:data => @fixedfloatswaps2.map {|x| [((((x.end_date)- Time.now.to_i)/ 31556926)), x.price_notation]})
       f.tooltip(:valueSuffix => "%")
       f.yAxis [
--- app/models/interest_swap.rb
+++ app/models/interest_swap.rb
@@ -7,9 +7,9 @@ class InterestSwap < ActiveRecord::Base
 
   def self.search(search)
     if search
-      self.where('taxonomy = ? AND execution_timestamp > ? AND end_date < ?', 'InterestRate:IRSwap:FixedFloat', (Time.now.to_i - 345600), "#{search}".to_i)
+      self.where('taxonomy = ? AND execution_timestamp > ? AND end_date < ? AND price_notation < ?', 'InterestRate:IRSwap:FixedFloat', (Time.now.to_i - 345600), "#{search}".to_i, 100)
     else
-      self.where('taxonomy = ? AND execution_timestamp > ?', 'InterestRate:IRSwap:FixedFloat', (Time.now.to_i - 345600))
+      self.where('taxonomy = ? AND execution_timestamp > ? AND price_notation < ?', 'InterestRate:IRSwap:FixedFloat', (Time.now.to_i - 345600), 100)
     end
   end
 
