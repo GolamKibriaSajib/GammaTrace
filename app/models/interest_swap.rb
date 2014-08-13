b68c0da34e90ee012213704459bffa7753c9adfe
abrarmusa <abrar.musa.89@gmail.com>
2014-08-13 01:10:21 -0700

    added scopes to model for swaps

--- app/models/interest_swap.rb
+++ app/models/interest_swap.rb
@@ -3,8 +3,9 @@ class InterestSwap < ActiveRecord::Base
   # Table connectors
   self.table_name = "interest_swap"
   self.primary_key = "dissemination_id"
-
-
+  scope :irfixedfloat, lambda {{:conditions =>
+    ['taxonomy = ? AND execution_timestamp > ? AND action = ? AND settlement_currency = ?  AND price_notation < ?AND indication_of_other_price_affecting_term = ?', 'InterestRate:IRSwap:FixedFloat', (Time.now.to_i - 345600), 'NEW', 'USD', 100, 'N']}}
+  # named_scope :t_or_t, lambda {{ :conditions => ['price_forming_continuation_data = ? OR price_forming_continuation_data = ?', 'Trade', 'Termination']}}
   def self.search(search)
     if search
       self.where('taxonomy = ? AND execution_timestamp > ? AND end_date < ? AND price_notation < ?', 'InterestRate:IRSwap:FixedFloat', (Time.now.to_i - 345600), "#{search}".to_i, 100)
---
+++ app/views/interest_swaps/index.js
@@ -0,0 +1,4 @@
+:javascript
+  function() {
+    return 'Extra data: <b>'+ this.point.dissemination_id +'</b>';
+  }
