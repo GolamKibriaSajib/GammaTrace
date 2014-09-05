8a71db5a3040cc969f322ba625e33bed040615c9
abrarmusa <abrar.musa.89@gmail.com>
2014-09-05 01:14:00 -0700

    styling changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,24 +16,32 @@
 //= require bootstrap
 //= require rickshaw_with_d3
 //= require highstock
-//= require grid-light
-//= require rwd-table
+//= require highchartstheme
 //= require jquery.draggable
 //= require jquery.gridster
 //= require utils
 //= require lodash
 //= require underscore
 //= require jquery.scrollTableBody
+//= require spin.min
 //= require turbolinks
 //= require_tree .
 
-
-$(function () {
-    Highcharts.setOptions({
-        chart: {
-            style: {
-                fontFamily: 'Raleway'
-            }
-        }
-    });
-})
+opts = {
+  lines: 17, // The number of lines to draw
+  length: 35, // The length of each line
+  width: 2, // The line thickness
+  radius: 60, // The radius of the inner circle
+  corners: 1, // Corner roundness (0..1)
+  rotate: 0, // The rotation offset
+  direction: 1, // 1: clockwise, -1: counterclockwise
+  color: 'orange', // #rgb or #rrggbb or array of colors
+  speed: 2, // Rounds per second
+  trail: 100, // Afterglow percentage
+  shadow: false, // Whether to render a shadow
+  hwaccel: false, // Whether to use hardware acceleration
+  className: 'spinner', // The CSS class to assign to the spinner
+  zIndex: 2e9, // The z-index (defaults to 2000000000)
+  top: '50%', // Top position relative to parent
+  left: '50%' // Left position relative to parent
+};
