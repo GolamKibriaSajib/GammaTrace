d70ddb5332b15f08045217e6bc3b92641096db0a
abrarmusa <abrar.musa.89@gmail.com>
2015-02-19 08:48:16 -0800

    added navigator for termstructure

--- app/assets/javascripts/widgetadder.js
+++ app/assets/javascripts/widgetadder.js
@@ -3,11 +3,9 @@
 function addWidget(graphType, gTypeName) {
   randId = chance.hash();
   if (graphType == "termstructure"){
-    gridster.add_widget('<li class="droppable panel panel-default" id="' + graphType + randId + '"><div class="panel-heading ' + graphType + ' dragDiv" id="searchname">' + gTypeName + '<div class="btn-group pull-right"><a href="javascript:void(0)" id="closer" class="closer">x</a></div></div><div class="panel-body" style="width:100%;height:45%;margin: 0 auto"></div><div class="panel-body second" style="width:100%;height:45%;margin: 0 auto"></div></li>', 6, 6);
+    gridster.add_widget('<li class="droppable panel panel-default" id="' + graphType + randId + '"><div class="panel-heading ' + graphType + ' dragDiv" id="searchname">' + gTypeName + '<div class="btn-group pull-right"><a href="javascript:void(0)" id="closer" class="closer">x</a></div></div><div class="panel-body" style="width:100%;height:90%;margin: 0 auto"></div></li>', 6, 6);
   } else {
      gridster.add_widget('<li class="droppable panel panel-default" id="' + graphType + randId + '"><div class="panel-heading ' + graphType + ' dragDiv" id="searchname">' + gTypeName + '<div class="btn-group pull-right"><a href="javascript:void(0)" id="closer" class="closer">x</a></div></div><div class="panel-body" style="width:100%;height:90%;margin: 0 auto"></div></li>', 6, 6);
   }
- 
-
   
 }
