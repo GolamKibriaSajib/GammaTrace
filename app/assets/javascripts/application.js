f0ff404800aecfe2d144d4b79fe74b239f41bceb
abrarmusa <abrar.musa.89@gmail.com>
2015-03-10 10:33:10 -0700

    incremental update 2

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,23 +15,11 @@
 //= require jquery_ujs
 //= require bootstrap
 //= require jquery-ui
-//= require chance.min
 //= require highstock
-//= require highchartstheme
-//= require jquery.draggable.min
-//= require graphloader
-//= require widgetadder
-//= require draggability.min
-//= require jquery.scrollTableBody
-//= require spin.min
-//= require livedataloader
-//= require jquery.qtip.min
 //= require angular
 //= require angular-animate
 //= require angular-resource
 //= require angular-ui-bootstrap
-//= require ng-app/gammatrace
-//= require ng-app/angular-gridster
 //= require_tree .
 
 opts={lines:17,length:35,width:2,radius:60,corners:1,rotate:0,direction:1,color:"orange",speed:2,trail:100,shadow:false,hwaccel:false,className:"spinner",zIndex:2e9,top:"50%",left:"50%"};$(function(){$(document).tooltip({show:{effect:"slideDown",delay:5}});$("#menunav").multilevelpushmenu({menuWidth:60,menuHeight:"100%",collapsed:false,backText:"",backItemIcon:"fa fa-angle-right fa-2x",overlapWidth:40})})
